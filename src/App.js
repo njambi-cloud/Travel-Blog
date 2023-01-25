6f5eba82fc4da1820084186508ec808b8f2748ba
njambi-cloud <christinemacharia64@gmail.com>
2023-01-25 12:00:18 +0300

    Creates the travel-blog app, adds the Navbar and About components and the routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import About from './About';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbar/>
+        <Routes>
+          <Route exact path="/about" element={<About/>}/>
+        </Routes>
+        
+        
+      Teddy
       </header>
     </div>
   );
