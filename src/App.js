fe41276f9ad4d87a226b77653da11ad0973fa717
njambi-cloud <christinemacharia64@gmail.com>
2023-01-26 08:54:59 +0300

    Adds the links and the routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import About from './About';
+import Home from './Home';
 import './App.css';
 import Navbar from './Navbar';
+import Destinations from './Destinations';
+import Works from './Works';
+import Travel from './Travel';
 
 function App() {
   return (
@@ -10,10 +14,11 @@ function App() {
       <Navbar/>
         <Routes>
           <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/destinations" element={<Destinations/>}/>
+          <Route exact path="/travel" element={<Travel/>}/>
+          <Route exact path="/works" element={<Works/>}/>
+          <Route exact path="/" element={<Home/>}/>
         </Routes>
-        
-        
-      Teddy
       </header>
     </div>
   );
