beba001d93e548165d9e6634db01b1e78ae18fcf
njambi-cloud <christinemacharia64@gmail.com>
2023-01-27 12:27:21 +0300

    Adds the display of the destinations

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
@@ -8,13 +9,25 @@ import Works from './Works';
 import Travel from './Travel';
 
 function App() {
+  const [dest,setDest] =useState([])
+  const [likes, setLikes] = useState(0)
+
+useEffect(()=>{
+    fetch("http://localhost:3000/destinations")
+        .then((res)=>res.json())
+        .then((dest)=> setDest(dest))
+},[])
+
+function handleClickLike (e) {
+  setLikes((prevLikes) => prevLikes + 1)
+}
   return (
     <div className="App">
       <header className="App-header">
       <Navbar/>
         <Routes>
           <Route exact path="/about" element={<About/>}/>
-          <Route exact path="/destinations" element={<Destinations/>}/>
+          <Route exact path="/destinations" element={<Destinations dest={dest} onAddLike={handleClickLike}/>}/>
           <Route exact path="/travel" element={<Travel/>}/>
           <Route exact path="/works" element={<Works/>}/>
           <Route exact path="/" element={<Home/>}/>
